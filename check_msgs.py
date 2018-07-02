5fc717eb69b36156a76d092e6542f7cf29c83662
hardiepienaar <hardiepienaar@gmail.com>
2018-07-02 20:08:29 +0100

    Plotting raw data

--- check_msgs.py
+++ check_msgs.py
@@ -15,14 +15,15 @@ import time
 port = "COM5"
 
 # Switch device to BINR
-print("Switching serial protocol to BINR")
-ser = serial.Serial(port, 115200)
-ser.write("$PORZA,0,115200,3*7E\r\n".encode())
-ser.close()
+# print("Switching serial protocol to BINR")
+# ser = serial.Serial(port, 115200)
+# ser.write("$PORZA,0,115200,3*7E\r\n".encode())
+# ser.close()
 
 # Connect to device
 print("Connecting to serial port [BINR 115200]")
-ser = serial.Serial(port, 115200, parity=serial.PARITY_ODD, timeout=0.1)
+#ser = serial.Serial(port, 115200, parity=serial.PARITY_ODD, timeout=0.1)
+ser = open("pelham_shed_1_July_2018.dat",'rb')
 
 # Request current receiver channels status
 """ print("Requesting receiver channels status")
@@ -38,7 +39,7 @@ buffer = ser.read(1000)
 data, buffer = binr.process_msg(buffer) """
 
 print("Requesting raw data stream")
-ser.write(binr.request_raw_data(50))
+# ser.write(binr.request_raw_data(50))
 buffer = []
 try:
     while True:
@@ -46,12 +47,13 @@ try:
             buffer = buffer + list(ser.read(1000))
             if len(buffer) > 0:
                 data, buffer = binr.process_msg(buffer)
-                print("Msg: "+str(hex(data["ID"]))+": "+
-                      str(len(data["data"]))+" bytes : "+
-                      str(bytearray(data["data"][0:50])))
+                # print("Msg: "+str(hex(data["ID"]))+": "+
+                #       str(len(data["data"]))+" bytes : "+
+                #       str(bytearray(data["data"][0:50])))
                 if data["ID"] == 0xF5:
                     raw_data = binr.process_raw_data(data["data"])
                     binr.print_raw_data(raw_data)
+            time.sleep(0.5)
         except ValueError:
                 s = 1
         time.sleep(0.1)
